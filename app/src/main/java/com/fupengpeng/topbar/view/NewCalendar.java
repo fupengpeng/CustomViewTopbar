57721eec4725fd9573dcebbe3e554960a616f149
fupengpeng <564055710@qq.com>
2017-05-23 18:05:23 +0800

    dds

---
+++ app/src/main/java/com/fupengpeng/topbar/view/NewCalendar.java
@@ -0,0 +1,25 @@
+package com.fupengpeng.topbar.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by fupengpeng on 2017/5/23 0023.
+ *     继承LinearLayout，自定义Layout
+ */
+
+public class NewCalendar extends LinearLayout{
+    public NewCalendar(Context context) {
+        super(context);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
